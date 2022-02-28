c16f9c9c1be0caaf659503c497eafa0e13055919
OuyangWei <18344175265@126.com>
2022-02-28 23:36:30 +0800

    update for windows performance optimize

--- Windows/WzSerialportPlus.cpp
+++ Windows/WzSerialportPlus.cpp
@@ -155,6 +155,8 @@ bool WzSerialportPlus::open()
 
             receivedLength = 0;
 			readState = false;
+			
+			std::this_thread::sleep_for(std::chrono::milliseconds(10));
         }
 
         delete[] receiveData;
